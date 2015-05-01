5f8aaa5d6a8fdba956ab6eb1f32bd1bd03c56c7b
nycucumber <yang.wang.dec@gmail.com>
2015-05-01 15:30:19 -0700

    got pulse sensor working

---
+++ Assets/heartBeatMe.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class heartBeatMe : MonoBehaviour {
+
+	Vector3 shrinkSpeed = new Vector3(0.1f,0.1f,0.1f);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Debug.Log (sensorInput.getSingleton().heartBeatValue);
+		if(sensorInput.getSingleton().heartBeatValue > 1000){
+			Vector3 tempScale = new Vector3(3,3,3);
+			gameObject.transform.localScale = tempScale;
+		}else{
+			if(gameObject.transform.localScale.magnitude > 1){
+				gameObject.transform.localScale -= shrinkSpeed;
+			}
+		}
+	}
+
+}
--- Assets/sensorInput.cs
+++ Assets/sensorInput.cs
@@ -5,28 +5,24 @@ using Uniduino;
 
 public class sensorInput : MonoBehaviour {
 
+	private static sensorInput singleton = null;
+
+	public static sensorInput getSingleton(){
+		return singleton;
+	}
+
 	public Arduino arduino;
 	public int breathSensorPin = 0;
-	volatile int Signal; //holds the incoming raw data
+	public int pulseBeatPin = 2;
+
+
+	public float heartBeatValue = 0.0f; 
 
 	List<float> rawDataList = new List<float>();
 	List<float> xList = new List<float>();
-
-//	//Interrupt
-////	volatile int rate[10];   
-//	volatile int[] rate;// array to hold last ten IBI values
-//	volatile float sampleCounter = 0.0f;          // used to determine pulse timing
-//	volatile float lastBeatTime = 0.0f;           // used to find IBI
-//	volatile int P =512;                      // used to find peak in pulse wave, seeded
-//	volatile int T = 512;                     // used to find trough in pulse wave, seeded
-//	volatile int thresh = 525;                // used to find instant moment of heart beat, seeded
-//	volatile int amp = 100;                   // used to hold amplitude of pulse waveform, seeded
-//	volatile bool firstBeat = true;        // used to seed rate array so we startup with reasonable BPM
-//	volatile bool secondBeat = false;      // used to seed rate array so we startup with reasonable BPM
-
-	// Use this for initialization
+	
 	void Start () {
-//		rate = new int[10];
+		singleton = this;
 		arduino = Arduino.global;
 		arduino.Setup(ConfigurePins);
 	}
@@ -34,24 +30,34 @@ public class sensorInput : MonoBehaviour {
 	void ConfigurePins( )
 	{
 		arduino.pinMode(0, PinMode.ANALOG);
+		arduino.pinMode(1, PinMode.ANALOG);
+		arduino.pinMode(2,PinMode.ANALOG);
 		arduino.reportAnalog(0,1); //report status
+		arduino.reportAnalog(1,1); //report statu
+		arduino.reportAnalog(2,1);
 	}
 
+	//for drawing debug line
 	float incrX = 0.0f;
 	float xIncrement = 0.4f;
 
 	// Update is called once per frame
 	void Update () {
-//		Debug.Log(arduino.analogRead(breathSensorPin));
-		rawDataList.Add(arduino.analogRead(breathSensorPin));
-		xList.Add(incrX);
-		incrX+=xIncrement;
+//		Debug.Log(arduino.analogRead(2));
 
-		for(int i=1; i< rawDataList.Count;i++){
-			Vector3 point_one = new Vector3(xList[i-1],rawDataList[i-1],0);
-			Vector3 point_two = new Vector3(xList[i],rawDataList[i],0);
-			Debug.DrawLine(point_one,point_two);
-		}
+		heartBeatValue = arduino.analogRead(pulseBeatPin);
+//
+//		//for drawing debug line
+//		rawDataList.Add(arduino.analogRead(1));
+//
+//		xList.Add(incrX);
+//		incrX+=xIncrement;
+//
+//		for(int i=1; i< rawDataList.Count;i++){
+//			Vector3 point_one = new Vector3(xList[i-1],rawDataList[i-1],0);
+//			Vector3 point_two = new Vector3(xList[i],rawDataList[i],0);
+//			Debug.DrawLine(point_one,point_two);
+//		}
 
 	}
 	
