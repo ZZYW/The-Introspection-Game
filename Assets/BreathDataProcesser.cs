60e20aa0915dcfa1c9637cfa1836a475f22fae15
nycucumber <yang.wang.dec@gmail.com>
2015-05-06 16:31:32 -0700

    my algorithm on detecing inhale/exhale is a big bullshit

--- Assets/BreathDataProcesser.cs
+++ Assets/BreathDataProcesser.cs
@@ -14,21 +14,35 @@ public class BreathDataProcesser : MonoBehaviour {
 	States STATE;
 	bool isStable = false;
 	bool firstLogOut = false, secondLogOut = false, thirdLogOut = false; //log control, make sure log will only be printed once
+	public bool isInhaling = false, isExhaling = false;
 	List<float> dataSetForFixAverage; //list for calculating fixed average
-	public List<float> processedDataList;
+	List<float> processedDataList;
 	public float dampRate = 0.06f;
 	public float totalAverage = 0.0f;
 	public float inhaleExhalePurposeAverage =0.0f;
 	int dataSetSize = 500;
 	int lengthOfListForGettingFixedAverage = 1000;
 	public float noiseRange = 50.0f;
-	
+
+	public bool isDecreasing = false, isIncreasing = false;
+	public int decreaseCounter = 0;
+	public int increaseCounter = 0;
+	int falutTolerantThreshold = 5;
+	int increaseFaultTolerantCounter = 0;
+	int decreaseFaultTolerantCounter = 0;
+	public int inhaleDetermineThreshold = 10;
+	public int exhaleDetermineThreshold = 8;
+	public int deepBreathDetermineThreshold = 25;
+	public int inhaleCounter = 0;
+	public int deepBreathCounter = 0;
+
 
 	void Start () {
 		processedDataList = new List<float>();
 		dataSetForFixAverage = new List<float>();
 	}
-	
+
+
 	void Update () {
 		//get data from sensorInput
 		float rawData = sensorInput.getSingleton().rawBreathingValue;
@@ -41,7 +55,11 @@ public class BreathDataProcesser : MonoBehaviour {
 			//smoother
 			float difference = rawData - processedDataList[processedDataList.Count-1];
 			processedData = processedDataList[processedDataList.Count-1] + ( difference * dampRate );
+			detectInExHale(processedData);
 			processedDataList.Add(processedData);
+
+		
+
 			//limit list's size to dataSetSize
 			if(processedDataList.Count > dataSetSize){
 				processedDataList.RemoveAt(0);
@@ -105,8 +123,8 @@ public class BreathDataProcesser : MonoBehaviour {
 	void drawCurves(){
 		if(processedDataList.Count>2){
 			for(int i=1; i< processedDataList.Count;i++){
-				Vector3 point_one = new Vector3(i-1,processedDataList[i-1]);
-				Vector3 point_two = new Vector3(i,processedDataList[i]);
+				Vector2 point_one = new Vector2(i-1,processedDataList[i-1]);
+				Vector2 point_two = new Vector2(i,processedDataList[i]);
 				Debug.DrawLine(point_one,point_two);
 			}
 		}
@@ -115,4 +133,66 @@ public class BreathDataProcesser : MonoBehaviour {
 			Debug.DrawLine(new Vector2(0,inhaleExhalePurposeAverage),new Vector2(processedDataList.Count,inhaleExhalePurposeAverage));
 		}
 	}
+
+
+	void detectInExHale(float newInput){
+
+
+		if(!isIncreasing && !isDecreasing){
+			if(newInput > processedDataList[processedDataList.Count-1]){
+				isIncreasing = true;
+			}else if(newInput < processedDataList[processedDataList.Count-1]){
+				isDecreasing = true;
+			}
+		}
+
+		if(isIncreasing){
+			if(newInput > processedDataList[processedDataList.Count-1]){
+				increaseCounter++;
+			}else if(newInput < processedDataList[processedDataList.Count-1]){
+				if(increaseFaultTolerantCounter < falutTolerantThreshold){
+					increaseFaultTolerantCounter++;
+				}else{
+					increaseCounter = 0;
+					isIncreasing = false;
+					increaseFaultTolerantCounter = 0;
+				}
+			}
+		}
+
+		if(isDecreasing){
+			if(newInput < processedDataList[processedDataList.Count-1]){
+				decreaseCounter++;
+			}else if(newInput > processedDataList[processedDataList.Count-1]){
+				if(decreaseFaultTolerantCounter < falutTolerantThreshold){
+					decreaseFaultTolerantCounter++;
+				}else{
+					decreaseCounter = 0;
+					isDecreasing = false;
+					decreaseFaultTolerantCounter = 0;
+				}
+
+			}
+		}
+
+		if(increaseCounter>=inhaleDetermineThreshold){
+			isExhaling = true;
+		}else if(increaseCounter < inhaleDetermineThreshold){
+			isExhaling = false;
+		}
+		if(decreaseCounter >= exhaleDetermineThreshold){
+			isInhaling = true;
+		}else if(decreaseCounter < exhaleDetermineThreshold){
+			isInhaling = false;
+		}
+
+//		if(increaseCounter>=deepBreathDetermineThreshold){
+//			deepBreathCounter++;
+//		}
+
+
+	}
+
+
+
 }
