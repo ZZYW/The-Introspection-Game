7d119e6aa51d2184797ff79d41270f130742db1a
nycucumber <yang.wang.dec@gmail.com>
2015-05-07 23:46:24 -0700

    inhale exhale detecing works pretty wellgit add .! moving to deep breath detection

--- Assets/UserScripts/BreathDataProcesser.cs
+++ Assets/UserScripts/BreathDataProcesser.cs
@@ -13,32 +13,37 @@ public class BreathDataProcesser : MonoBehaviour {
 	public delegate void del();
 	public static event del breathSensorReady;
 
+	//state control
 	States STATE;
-
 	bool isStable = false;
 	bool firstLogOut = false, secondLogOut = false, thirdLogOut = false; //log control, make sure log will only be printed once
-	public bool isInhaling = false, isExhaling = false;
+
+
+	//data process
 	List<float> dataSetForFixAverage; //list for calculating fixed average
 	List<float> processedDataList;
-	public float dampRate = 0.06f;
-	public float totalAverage = 0.0f;
-	public float inhaleExhalePurposeAverage =0.0f;
+	float dampRate = 0.03f;
+	float inhaleExhalePurposeAverage =0.0f;
 	int dataSetSize = 500;
 	int lengthOfListForGettingFixedAverage = 1000;
-	public float noiseRange = 50.0f;
 
-	public bool isDecreasing = false, isIncreasing = false;
-	public int decreaseCounter = 0;
-	public int increaseCounter = 0;
-	int falutTolerantThreshold = 5;
+
+	//detect inhale/exhale
+	public bool isInhaling = false, isExhaling = false;
+	bool isDecreasing = false, isIncreasing = false;
+	int decreaseCounter = 0;
+	int increaseCounter = 0;
+	int falutTolerantThreshold = 10; //how many times we ignore before swtich states between increasing and decrease
 	int increaseFaultTolerantCounter = 0;
 	int decreaseFaultTolerantCounter = 0;
 	public int inhaleDetermineThreshold = 10;
 	public int exhaleDetermineThreshold = 8;
-	public int deepBreathDetermineThreshold = 25;
+	public int deepBreathDetermineThreshold = 85;
 	public int inhaleCounter = 0;
 	public int deepBreathCounter = 0;
 
+
+	//data viz
 	public bool drawCurve = true;
 
 	void Start () {
@@ -53,24 +58,11 @@ public class BreathDataProcesser : MonoBehaviour {
 		float processedData = 0.0f;
 		processedData = SensorProcessingMethods.smoothDataAddToList(processedDataList, rawData, dampRate ,dataSetSize);
 
-//		//get at least one data into the list
-//		if(processedDataList.Count == 0){
-//			processedDataList.Add(rawData);
-//		}else{
-//			//smoother
-//			float difference = rawData - processedDataList[processedDataList.Count-1];
-//			processedData = processedDataList[processedDataList.Count-1] + ( difference * dampRate );
-//			detectInExHale(processedData);
-//			processedDataList.Add(processedData);
-//
-//		
-//
-//			//limit list's size to dataSetSize
-//			if(processedDataList.Count > dataSetSize){
-//				processedDataList.RemoveAt(0);
-//			}
-//		}
-
+		//start detecing data when actually data comes in
+		if(STATE!=States.WAITING_ACTUALLY_SENSOR_INPUT){
+			detectInExHale(processedData);
+		}
+	
 		//state control
 		//super niubi!
 
@@ -128,67 +120,86 @@ public class BreathDataProcesser : MonoBehaviour {
 				SensorProcessingMethods.drawCurves(processedDataList,inhaleExhalePurposeAverage,isStable);
 		}
 	}
-
-
-
+	
 
 	void detectInExHale(float newInput){
 
+//		if(processedDataList.Count>=2){ //rocessedDataList[processedDataList.Count-2]
 
-		if(!isIncreasing && !isDecreasing){
-			if(newInput > processedDataList[processedDataList.Count-1]){
-				isIncreasing = true;
-			}else if(newInput < processedDataList[processedDataList.Count-1]){
-				isDecreasing = true;
-			}
-		}
-
-		if(isIncreasing){
-			if(newInput > processedDataList[processedDataList.Count-1]){
-				increaseCounter++;
-			}else if(newInput < processedDataList[processedDataList.Count-1]){
-				if(increaseFaultTolerantCounter < falutTolerantThreshold){
-					increaseFaultTolerantCounter++;
-				}else{
-					increaseCounter = 0;
-					isIncreasing = false;
-					increaseFaultTolerantCounter = 0;
+			//when neither increasing nor decreasing
+			if(!isIncreasing && !isDecreasing){
+				//if the new data is greater than the previous one, then turn on Increasing flag. 
+				if(newInput > processedDataList[processedDataList.Count-2]){
+					isIncreasing = true;
+				}else if(newInput < processedDataList[processedDataList.Count-2]){
+				//if the new data is greater than the previous one, then turn on Increasing flag. 
+					isDecreasing = true;
 				}
 			}
-		}
 
-		if(isDecreasing){
-			if(newInput < processedDataList[processedDataList.Count-1]){
-				decreaseCounter++;
-			}else if(newInput > processedDataList[processedDataList.Count-1]){
-				if(decreaseFaultTolerantCounter < falutTolerantThreshold){
-					decreaseFaultTolerantCounter++;
-				}else{
-					decreaseCounter = 0;
-					isDecreasing = false;
-					decreaseFaultTolerantCounter = 0;
+
+	/* if increasing(exhale) flag is On.
+	 * verify if next coming data is still smaller than its previous data
+	 * if YES, then add up 1 into increasing counter,
+	 * if NO && increaseFaultTolerantCounter hasn't reached the falutTolerantThreshold
+	 * then add one to increaseFaultTolerantCounter
+	 * If increaseFaultTolerantCounter has already reached the falutTolerantThreshold
+	 * then turn increasing flag to false and clear increaseFaultTolerantCounter and increaseCounter to 0
+	 * Decreasing(inhale) works exactly the same way.
+				 */
+
+			if(isIncreasing){
+				if(newInput > processedDataList[processedDataList.Count-2]){
+					increaseCounter++;
+	 
+				}else if(newInput < processedDataList[processedDataList.Count-2]){ 
+					if(increaseFaultTolerantCounter < falutTolerantThreshold){
+						increaseFaultTolerantCounter++;
+					}else{
+						increaseCounter = 0;
+						isIncreasing = false;
+						increaseFaultTolerantCounter = 0;
+					}
 				}
-
 			}
+
+			if(isDecreasing){
+				if(newInput < processedDataList[processedDataList.Count-2]){
+					decreaseCounter++;
+				}else if(newInput > processedDataList[processedDataList.Count-2]){
+					if(decreaseFaultTolerantCounter < falutTolerantThreshold){
+						decreaseFaultTolerantCounter++;
+					}else{
+						decreaseCounter = 0;
+						isDecreasing = false;
+						decreaseFaultTolerantCounter = 0;
+					}
+
+				}
+			}
+			
+			//by knowing the level of the increasing/decreasing, we know wheather the user is inhaling or exhaling.
+			//the inhaleDetermineThreshold & exhaleDetermineThreshold is a small number that used to filter noise.
+
+			if(increaseCounter>=inhaleDetermineThreshold){
+				isExhaling = true;
+			}else if(increaseCounter < inhaleDetermineThreshold){
+				isExhaling = false;
+			}
+			if(decreaseCounter >= exhaleDetermineThreshold){
+				isInhaling = true;
+			}else if(decreaseCounter < exhaleDetermineThreshold){
+				isInhaling = false;
+			}
+
+			//if the decreasing is deeper than a certain threshold, then we add one to the deep breath counter.
+			if(decreaseCounter>=deepBreathDetermineThreshold){
+				deepBreathCounter++;
+			}
+
 		}
 
-		if(increaseCounter>=inhaleDetermineThreshold){
-			isExhaling = true;
-		}else if(increaseCounter < inhaleDetermineThreshold){
-			isExhaling = false;
-		}
-		if(decreaseCounter >= exhaleDetermineThreshold){
-			isInhaling = true;
-		}else if(decreaseCounter < exhaleDetermineThreshold){
-			isInhaling = false;
-		}
-
-//		if(increaseCounter>=deepBreathDetermineThreshold){
-//			deepBreathCounter++;
-//		}
-
-
-	}
+//	}
 
 
 
